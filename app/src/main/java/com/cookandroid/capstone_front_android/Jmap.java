4be524b29e59e532f0ca55cc35644a34029754a7
DDong17 <ghkdehdgh11@gmail.com>
2022-05-18 15:29:04 +0900

    경찬님 pr과 Dong 합쳐서 master로 pr

--- app/src/main/java/com/cookandroid/capstone_front_android/Jmap.java
+++ app/src/main/java/com/cookandroid/capstone_front_android/Jmap.java
@@ -1,5 +1,4 @@
 package com.cookandroid.capstone_front_android;
-
 import android.Manifest;
 import android.app.AlertDialog;
 import android.app.FragmentManager;
@@ -18,13 +17,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-
 import com.cookandroid.capstone_front_android.member.model.MemberApi;
 import com.cookandroid.capstone_front_android.network.RetrofitClient;
 import com.google.android.gms.maps.CameraUpdate;
@@ -38,102 +35,102 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-
 public class Jmap extends Fragment implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
     private final MemberApi memberApi = RetrofitClient.getClient(MemberApi.class, RetrofitClient.getSessionId());
     private View view;
     TextView text;
-
     private MapView sView;
-
     LocationManager lm;
     Location location;
     String provider;
     double longitude = 0;
     double latitude = 0;
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.map, container, false);
         text = view.findViewById(R.id.txt);
-
         lm = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
-
         if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             Toast.makeText(getActivity(), "GPS OFF상태", Toast.LENGTH_SHORT).show();
             return view;
         }
-
         location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
         if(location == null){
             location = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
         }
-        provider = location.getProvider();
-        longitude = location.getLongitude();
-        latitude = location.getLatitude();
-        text.setText("위치정보 : " + provider + "\n" + "위도 : " + longitude + "\n" + "경도 : " + latitude);
 
-        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, gpsLocationListener);
-        lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 1, gpsLocationListener);
+
+        // last known location 이 위치를 제대로 가져왔는지 확인
+        if(location == null) { // 위치 확인 안 되는 경우
+            Toast.makeText(getActivity(), "마지막 위치 가져오기 실패", Toast.LENGTH_SHORT).show();
+
+            //location = new Location(LocationManager.NETWORK_PROVIDER);
+            provider = "null";
+            longitude = 0;
+            latitude = 0;
+            text.setText("위치정보 확인 불가");
+        } else { // 위치 확인 성공한 경우
+            provider = location.getProvider();
+            longitude = location.getLongitude();
+            latitude = location.getLatitude();
+            text.setText("위치정보 : " + provider + "\n" + "위도 : " + longitude + "\n" + "경도 : " + latitude);
+
+            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, gpsLocationListener);
+            lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 1, gpsLocationListener);
+        }
+
+        /*try {
+            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, gpsLocationListener);
+            lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 1, gpsLocationListener);
+        } catch(IllegalArgumentException e) {
+            Toast.makeText(getActivity(), "위치 업데이트 실패", Toast.LENGTH_SHORT).show();
+        }*/
 
         sView = (MapView) view.findViewById(R.id.sMap);
         sView.onCreate(savedInstanceState);
         sView.getMapAsync(this);
-
         return view;
     }
-
     final LocationListener gpsLocationListener = new LocationListener() {
         public void onLocationChanged(Location location) {
             provider = location.getProvider();
             longitude = location.getLongitude();
             latitude = location.getLatitude();
-
             text.setText("위치정보 : " + provider + "   " + "위도 : " + latitude + "\n" + "경도 : " + longitude);
         }
-
         public void onStatusChanged(String provider, int status, Bundle extras) {
         }
-
         public void onProviderEnabled(String provider) {
         }
-
         public void onProviderDisabled(String provider) {
         }
     };
-
     @Override
     public void onStart() {
         super.onStart();
         sView.onStart();
     }
-
     @Override
     public void onStop () {
         super.onStop();
         sView.onStop();
-
     }
-
     @Override
     public void onSaveInstanceState (@Nullable Bundle outState){
         super.onSaveInstanceState(outState);
         sView.onSaveInstanceState(outState);
     }
-
     @Override
     public void onResume() {
         super.onResume();
         sView.onResume();
     }
-
     @Override
     public void onDestroy() {
         super.onDestroy();
         sView.onLowMemory();
     }
-
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         //마커찍기(위도,경도)
@@ -145,17 +142,13 @@ public class Jmap extends Fragment implements OnMapReadyCallback, GoogleMap.OnMa
         //marker.title("역");
         //marker.snippet("오산역");
         marker.position(solnae); //마커 위치.
-
         //맵에 마커표시, 인포윈도우 보여줌.
         googleMap.addMarker(marker);
-
         //인포윈도우 클릭
         googleMap.setOnMarkerClickListener(this);
-
         //맵뷰 카메라위치, 줌 설정
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(solnae, 16));
     }
-
     @Override
     public boolean onMarkerClick(@NonNull Marker marker) {
         return false;
